1aa647505a776f76c15dd347ab9c7a0499718a8c
XuDaojie <xudaojieg@gmail.com>
2017-03-14 23:52:31 +0800

    Java

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,8 +1,7 @@
-/**
- * Created by xdj on 2017/3/14.
- */
+/** Created by xdj on 2017/3/14. */
 public class Main {
-    public static void main(String[] args) {
 
-    }
+  public static void main(String[] args) {
+    System.out.print("Hello World!");
+  }
 }
